2fe0e2bf3753ac86407b1b84a222b4281ed7a28f
Priysha <priysha2911@gmail.com>
2026-07-13 10:50:07 +0530

    Point frontend to live backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://tataevlocatorbackend-zftc9kq6.b4a.run/",
 });
 
 api.interceptors.request.use((config) => {
